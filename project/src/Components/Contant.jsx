3db02ee524622a1717804dfaf906f5dd33d614ca
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-03-03 23:54:45 +0530

    updated

--- project/src/Components/Contant.jsx
+++ project/src/Components/Contant.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import img from "../assets/img.jpg";
-import img2 from '../assets/IMAGE.jpg'
+import img from "../assets/system.jpg";
+import img2 from "../assets/rating-5.jpg";
 import "./contant.css";
 const Contant = () => {
   return (
@@ -16,18 +16,48 @@ const Contant = () => {
           <img src={img} alt="" />
           <h6>Builder 1</h6>
         </div>
-        <div className="mid-item">
+        <div className="mid-item cdk">
           <div className="about">
             <span>WixPro 72-Inch Responsive Website Builder</span>-
             Comprehensive Digital Platform Creation Tool, Streamlined Design
             Interface for Professional Websites and Online Stores (Black/Blue)
           </div>
-          <h5>Main highlights</h5>
-          <p>
+          {/* <h5>Main highlights</h5> */}
+          {/* <p>
             [What You Get]: Receive the WixPro website builder suite, access to
             premium design templates, an extensive library of widgets and apps,
             and detailed step-by-step guides.
-          </p>
+          </p> */}
+		  {/* The .cdj-details determine when it is going to show */}
+          <div className="cdk-details">
+			<div className="percentage">26% Off</div>
+            <h5>Main highlights</h5>
+            <div className="cdk-rate">
+              <div>
+                <span>9.9</span>&nbsp;building responsive
+              </div>
+              <div>
+                <span>8.9</span>&nbsp;Cool
+              </div>
+              <div>
+                <span>8.9</span>&nbsp;Docs
+              </div>
+            </div>
+            <div className="cdk-review">
+              <h6>Why we love it</h6>
+              <div className="reviews">
+                <div>
+                  <i class="bi bi-check-lg"></i>&nbsp;Documentation
+                </div>
+                <div>
+                  <i class="bi bi-check-lg"></i>&nbsp;Easy Use
+                </div>
+                <div>
+                  <i class="bi bi-check-lg"></i>&nbsp;Out of box
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="see-more">
             Show more&nbsp;<i className="bi bi-chevron-down"></i>
           </div>
@@ -35,10 +65,10 @@ const Contant = () => {
         <div className="right-item">
           <div className="rating-card">
             <div className="no">9.8</div>
-			<p>Exceptional</p>
-			<img src={img2} alt="" />
+            <p>Exceptional</p>
+            <img src={img2} alt="" />
           </div>
-		  <button>View</button>
+          <button>View</button>
         </div>
       </div>
     </div>
