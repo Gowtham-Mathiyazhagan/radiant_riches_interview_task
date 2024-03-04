7b2bcd840c3abce171dbf9178461b68621ba8438
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-03-04 13:58:11 +0530

    updated

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Headsection from './Components/Headsection'
-import Contant from './Components/Contant'
-import ContantTwo from './Components/ContantTwo'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Headsection from "./Components/Headsection";
+import Contant from "./Components/Contant";
+import ContantTwo from "./Components/ContantTwo";
+import SignUp from "./Components/SignUp";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-   {/* Wrapper used to make a contant center */}
-    <div className="wrapper"> 
-    <Headsection/>
-    <Contant/>
-    <ContantTwo/>
-    </div>
+      <Navbar />
+      {/* Wrapper used to make a contant center */}
+      <div className="wrapper">
+        <Headsection />
+        <Contant />
+        <ContantTwo />
+        <SignUp />
+        {/* <Footer/> */}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ project/src/Components/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './signUp.css'
+const SignUp = () => {
+  return (
+	<div className="signup-component">
+		<h4>
+		Sign up and get exclusive <br /> special deals
+		</h4>
+		<div className="input">
+			<input type="text" />
+			<button>Sign Up</button>
+		</div>
+	</div>
+  )
+}
+
+export default SignUp
