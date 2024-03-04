bd261b4e87d155ac609c3bd9978a5f39da1f6cee
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-03-04 12:17:01 +0530

    updated

---
+++ project/src/Components/Contant-two.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contant-two = () => {
+  return (
+	<div>Contant-two</div>
+  )
+}
+
+export default Contant-two
--- project/src/Components/Contant.jsx
+++ project/src/Components/Contant.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import "./contant.css";
-import { contantData } from "./data.js";
+import { contantData } from "../data/contant-one.js";
 const Contant = () => {
   return (
     <div className="contant-container">
       {/* Splited by left, mid, right */}
-      {contantData.map((val,i)=>{
-return(
-  <div className="item">
-        <div className="left-item">
-          <div className="label">
-            <i className={val.trophy}></i>
-            &nbsp; {val.label}
-          </div>
-          <div className="no">{val.no}</div>
-          <img src={val.system} alt="" />
-          <h6>{val.builder}</h6>
-        </div>
-        <div className="mid-item cdk">
-          <div className="about">
-            <span>{val.span}</span>{val.about}</div>
-          <h5>Main highlights</h5>
-          <p>
-           {val.p}
-          </p>
-          {/* The .cdj-details determine when it is going to show */}
-          {/* <div className="cdk-details">
+      {contantData.map((val, i) => {
+        return (
+          <div className="item">
+            <div className="left-item">
+              <div className="label" style={val.label==="none" ? {display:"none"}: {display:"block"}}>
+                <i className={val.trophy}></i>
+                &nbsp; {val.label}
+              </div>
+              <div className="no-and-img">
+              <div className="no">{val.no}</div>
+              <img src={val.system} alt="" />
+              <h6>{val.builder}</h6>
+              </div>
+            </div>
+            <div className={val.builder=="CDK" ? "mid-item cdk" : "mid-item"}>
+              <div className="about">
+                <span>{val.span}</span>
+                {val.about}
+              </div>
+              <h5 style={val.builder==="CDK" ? {display:"none"}: {display:"block"}}>Main highlights</h5>
+              <p style={val.builder==="CDK" ? {display:"none"}: {display:"block"}} >{val.p}</p>
+              {/* The .cdj-details determine when it is going to show */}
+              <div className="cdk-details" style={val.builder==="CDK" ? {display:"block"}: {display:"none"}}>
 			<div className="percentage">26% Off</div>
             <h5>Main highlights</h5>
             <div className="cdk-rate">
@@ -53,22 +55,21 @@ return(
                 </div>
               </div>
             </div>
-          </div> */}
-          <div className="see-more">
-            Show more&nbsp;<i className="bi bi-chevron-down"></i>
           </div>
-        </div>
-        <div className="right-item">
-          <div className="rating-card">
-            <div className="no">{val.rating_no}</div>
-            <p>{val.rating_p}</p>
-            <img src={val.img} alt={val.builder} />
+              <div className="see-more">
+                Show more&nbsp;<i className="bi bi-chevron-down"></i>
+              </div>
+            </div>
+            <div className="right-item">
+              <div className="rating-card">
+                <div className="no">{val.rating_no}</div>
+                <p>{val.rating_p}</p>
+                <img src={val.img} alt={val.builder} />
+              </div>
+              <button>View</button>
+            </div>
           </div>
-          <button>View</button>
-        </div>
-      </div>
-)
-
+        );
       })}
     </div>
   );
