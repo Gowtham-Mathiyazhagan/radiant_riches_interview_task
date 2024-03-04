d4aa68066852a4fc97f5b426f2f98aa00f6d5e72
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-03-04 13:33:31 +0530

    updated

--- project/src/Components/ContantTwo.jsx
+++ project/src/Components/ContantTwo.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import './contantTwo.css'
+import React from "react";
+import "./contantTwo.css";
+import { cardData } from "../data/contant-two";
 
 const ContantTwo = () => {
   return (
     <div className="contantTwo-container">
-       <h1>
-       Related deals you might like for
-       </h1>
+      <h1>Related deals you might like for</h1>
+      <div className="cards">
+        {cardData.map((data,i)=>{
+          return(
+<div key={i.toString()} className="card">
+          <img src={data.img} alt="" />
+          <div className="dicount">
+            <span>{data.dic}</span>
+            <span>{data.time}</span>
+          </div>
+          <h6>{data.build}</h6>
+          <p>{data.p}</p>
+          <div className="rate">
+            <span>{data.rate1}</span>
+            <span>{data.rate2}</span>
+            <span>{data.rate3}</span>
+          </div>
+          <button>View</button>
+        </div>
+          )
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContantTwo
+export default ContantTwo;
---
+++ project/src/data/contant-two.js
@@ -0,0 +1,34 @@
+import system from "../assets/system.jpg";
+
+export const cardData =[
+	{
+       img : system,
+	   dic : "20% Off",
+	   time : "Limited time",
+	   build :"Webbuilder 1",
+	   p:"Computer Modern clasic with wix support",
+	   rate1 : "$39.96",
+	   rate2: "$49.96",
+	   rate3 :"(20% Off)"
+	},
+	{
+		img : system,
+		dic : "20% Off",
+		time : "Limited time",
+		build :"Webbuilder 1",
+		p:"Computer Modern clasic with wix support",
+		rate1 : "$39.96",
+		rate2: "$49.96",
+		rate3 :"(20% Off)"
+	 },
+	 {
+		img : system,
+		dic : "20% Off",
+		time : "Limited time",
+		build :"Webbuilder 1",
+		p:"Computer Modern clasic with wix support",
+		rate1 : "$39.96",
+		rate2: "$49.96",
+		rate3 :"(20% Off)"
+	 },
+]
